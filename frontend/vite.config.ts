5da250adc3ec30ebc0cf54df718971b04cc649f5
putrasempalas1222-eng <putrasempalas1222@gmail.com>
2026-05-26 19:27:36 +0700

    Add files via upload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
     return {
+      base: './',
       server: {
         proxy: {
           '/api/chat': 'http://localhost:5000',
